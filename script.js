53d763f6e8f5208943c226ddb3e2e61a8e60add3
Erfan Mohammadi <erfanmohammadi7133@gmail.com>
2025-10-28 09:21:55 +0330

    s

--- script.js
+++ script.js
@@ -12,4 +12,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {anchor.addEventList
 });
 });
 });
+const sections =
+document.querySelectorAll('section');
+window.addEventListener('scroll', () => {
+    sections.forEach(sec => {
+        const rect =
+        sec.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) {
+            sec.classList.add('visible');
+        }
+    });
+});
 
